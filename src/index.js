c7fa3a9b50060a08f6af197ac90a4108e1848ba9
YX <pig60214@gmail.com>
2023-08-20 07:32:16 +0800

    Redux Persist In localStorage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { RouterProvider } from "react-router-dom";
 import router from './route';
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import searchInputSlice from './searchInputSlice'
 import locationsSlice from './locationsSlice';
 import toGivesSlice from './toGivesSlice';
 import showGiftListSlice from './showGiftListSlice';
 import toDoListSlice from './toDoListSlice';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
-export default configureStore({
-  reducer: {
-    searchInput: searchInputSlice,
-    locations: locationsSlice,
-    toGives: toGivesSlice,
-    showGiftList: showGiftListSlice,
-    toDoList: toDoListSlice,
-  },
+const rootReducer = combineReducers({
+  searchInput: searchInputSlice,
+  locations: locationsSlice,
+  toGives: toGivesSlice,
+  showGiftList: showGiftListSlice,
+  toDoList: toDoListSlice,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
 })
+
+export const persistor = persistStore(store);
