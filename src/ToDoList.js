4f0aed936ba731fc43dc1b69f2c1de271800e658
YX <pig60214@gmail.com>
2023-08-20 15:25:20 +0800

    Toggle ToGive And Location In ToDoList

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { removeToDo } from "./store/toDoListSlice";
+import { toggleLocation, toggleGoToShopping, toggleGoToGiveTheGift } from './store/locationsSlice';
+import { toggleToGive } from "./store/toGivesSlice";
 
-function ToDo({ content, children }) {
+function ToDo({ content, children, className, onClick }) {
   return (
-    <li className="bg-stone-400 rounded-lg p-4">
+    <li className={`bg-stone-300 rounded-lg p-4 ${className}`} onClick={onClick}>
       {content}
       {children}
     </li>
@@ -11,16 +13,30 @@ function ToDo({ content, children }) {
 }
 
 export default function ToDoList() {
-  const toGives = useSelector((state) => state.toGives).map(toGive => `送[${toGive.neighborhood}][${toGive.gift}]`);
-  const locations = useSelector((state) => state.locations).map(loc => `去[${loc.name}][${loc.shopping ? '買東西' : ''}${loc.toGive ? '送禮物' : ''}]`);
+  const toGives = useSelector((state) => state.toGives);
+  const locations = useSelector((state) => state.locations).map(loc => {
+    const { name, shopping, toGive } = loc;
+
+    if (!shopping && !toGive) {
+      return <div className="cursor-pointer" onClick={() => dispatch(toggleLocation(name))}>去{name}</div>
+    }
+
+    const shoppingDiv = <span className='bg-sky-400 rounded p-1 cursor-pointer' onClick={() => dispatch(toggleGoToShopping(name))}>買東西/找NPC</span>;
+    const toGiveDiv = <span className='bg-pink-400 rounded p-1 cursor-pointer' onClick={() => dispatch(toggleGoToGiveTheGift(name))}>送禮物</span>;
+    if (shopping && toGive) {
+      return <div>去{name}{shoppingDiv}{toGiveDiv}</div>;
+    }
+
+    return <div>去{name}{shopping ? shoppingDiv : toGiveDiv}</div>
+  });
   const toDoList = useSelector((state) => state.toDoList);
   const dispatch = useDispatch();
 
   return (
     <ul className="flex flex-wrap gap-2">
-      {toGives.map(todo => <ToDo key={todo} content={todo} />)}
-      {locations.map(todo => <ToDo key={todo} content={todo} />)}
-      {toDoList.map(todo => <ToDo key={todo} content={todo}><button className='w-8' onClick={() => dispatch(removeToDo(todo))}>X</button></ToDo>)}
+      {toGives.map((toGive, index) => <ToDo className="cursor-pointer" key={index} content={`送[${toGive.neighborhood}][${toGive.gift}]`} onClick={() => dispatch(toggleToGive(toGive))} />)}
+      {locations.map((todo, index) => <ToDo key={index}>{todo}</ToDo>)}
+      {toDoList.map(todo => <ToDo className="cursor-pointer" key={todo} content={todo} onClick={() => dispatch(removeToDo(todo))} />)}
     </ul>
   );
 }
