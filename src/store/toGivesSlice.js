eee0bbd0b83cc131f753bce9cd53201091455bd7
YX <pig60214@gmail.com>
2023-08-29 02:06:35 +0800

    TODO: Order ToGive By Gift Level

--- src/store/toGivesSlice.js
+++ src/store/toGivesSlice.js
@@ -9,13 +9,29 @@ export const toGivesSlice = createSlice({
       const { neighborhood, level, gift } = payload;
 
       if(state.find(toGive => toGive.neighborhood === neighborhood && toGive.gift === gift)) {
-        const next = state.filter(toGive => !(toGive.neighborhood === neighborhood && toGive.gift === gift));
-        return next;
+        return state.filter(toGive => !(toGive.neighborhood === neighborhood && toGive.gift === gift));
       } else {
-        return [
-          ...state,
-          {neighborhood, level, gift},
-        ];
+        const start = state.findIndex(toGive => toGive.neighborhood === neighborhood);
+        if (start === -1) {
+          return [
+            ...state,
+            {neighborhood, level, gift},
+          ];
+        }
+        const end = state.findLastIndex(toGive => toGive.neighborhood === neighborhood);
+        const before = state.slice(0, start);
+        const toSort = state.slice(start, end+1);
+        const after = state.slice(end+1, state.length);
+
+        toSort.push({neighborhood, level, gift});
+        toSort.sort((x, y) => {
+          if(x.level > y.level) return 1;
+          if(x.level === y.level) return 0;
+          return -1;
+        });
+
+        const next = before.concat(toSort).concat(after);
+        return next;
       }
     }
   },
