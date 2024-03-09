9bd3667cdad5587a603b21516e505d9f29362b00
YX <pig60214@gmail.com>
2024-03-09 06:56:49 -0500

    Adjust <select> CSS

--- src/views/Item.js
+++ src/views/Item.js
@@ -46,39 +46,37 @@ export default function Item() {
   const panel = (
     <ul className='my-tabs'>
       { ['菇', '蜂蜜', '木頭', '草', '石頭', '全品項'].map(c => <li key={c} className={setting.category === c || setting.category === '全品項' ? 'active' : 'inactive'} onClick={() => {selectCategory(c)}}>{lang(c)}</li>) }
-      <li>
-        <select name="category" onChange={(e) => {selectCategory(e.target.value)}} value={setting.category}>
-          <option value='全品項'>{lang('全品項')}</option>
-          {
-            [
-              { big: '草', small: ['草', '線', '布料']},
-              { big: '石頭', small: ['石頭', '金屬塊', '寶石']},
-              { big: '木頭', small: ['木頭', '木材']},
-              { big: '菇', small: ['菇', '種菌']},
-              { big: '蜂蜜'},
-              { big: '毛', small: ['毛', '布料']},
-              { big: '花卉', small: ['香精', '香水', '花束', '染料']},
-              { big: '開拓', small: ['山菜', '香草']},
-              { big: '烹飪', small: ['調味料', '果醬']},
-              { big: '釣魚', small: ['釣餌', '捕魚網']},
-              { big: '博物館', small: ['寶物', '寶物仿製品', '動物雕像']},
-              { big: '飾品'},
-              { big: '加工機'},
-              { big: '寵物用品'},
-              { big: '家具'},
-            ].map(bc => {
-              if(!bc.small) {
-                return <option key={bc.big} value={bc.big}>{lang(bc.big)}</option>
-              }
-              return (
-                <optgroup key={bc.big} label={lang(bc.big)}>
-                  {bc.small.map(sc => <option key={sc} value={sc}>{lang(sc)}</option>)}
-                </optgroup>
-              )
-            })
-          }
-        </select>
-      </li>
+      <select name="category" onChange={(e) => {selectCategory(e.target.value)}} value={setting.category}>
+        <option value='全品項'>{lang('全品項')}</option>
+        {
+          [
+            { big: '草', small: ['草', '線', '布料']},
+            { big: '石頭', small: ['石頭', '金屬塊', '寶石']},
+            { big: '木頭', small: ['木頭', '木材']},
+            { big: '菇', small: ['菇', '種菌']},
+            { big: '蜂蜜'},
+            { big: '毛', small: ['毛', '布料']},
+            { big: '花卉', small: ['香精', '香水', '花束', '染料']},
+            { big: '開拓', small: ['山菜', '香草']},
+            { big: '烹飪', small: ['調味料', '果醬']},
+            { big: '釣魚', small: ['釣餌', '捕魚網']},
+            { big: '博物館', small: ['寶物', '寶物仿製品', '動物雕像']},
+            { big: '飾品'},
+            { big: '加工機'},
+            { big: '寵物用品'},
+            { big: '家具'},
+          ].map(bc => {
+            if(!bc.small) {
+              return <option key={bc.big} value={bc.big}>{lang(bc.big)}</option>
+            }
+            return (
+              <optgroup key={bc.big} label={lang(bc.big)}>
+                {bc.small.map(sc => <option key={sc} value={sc}>{lang(sc)}</option>)}
+              </optgroup>
+            )
+          })
+        }
+      </select>
     </ul>
   )
 
