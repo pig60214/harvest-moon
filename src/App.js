12103a6f2ae97dcca5afc7b701d40163f7930b54
YX <pig60214@gmail.com>
2025-10-29 23:30:41 +0800

    Try Adjust Google Search Title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Outlet, Link, useLocation, useMatch, useResolvedPath } from "react-router-dom";
 import ToolButtons from 'components/ToolButtons';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import lang from 'rawData/resourse';
 import { Store } from 'react-notifications-component';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,32 +10,28 @@ import { gaEventTracker } from 'GA';
 
 function PageDescription() {
   const location = useLocation();
-  const [ showDescription, setShowDescription ] = useState();
   const descriptions = {
     '/neighbor': {
       'title': '牧場物語橄欖鎮 | 居民喜好 | 讓你送禮不再耗腦',
       // 'h1': '不知道居民喜好嗎？知道喜好但背包一堆禮物，配對困難嗎？讓禮物小助手協助你吧',
       // 'h2': '禮物小助手說明',
-      // 'p': '這是一個為了不考驗自己記憶力而生的功能，上方可以搜尋居民名字、居民描述或想送出的物品，快速找到想要的配對。點選物品後，小助手會幫你記錄起來，接著可以點擊出現在右手邊的禮物按鈕一覽要送出的配對。🩶：可結婚的居民，🩷：要送禮物給這位可結婚的居民🧡：要送禮給這位不可結婚居民'
     },
     '/crop': {
+      'title': '牧場物語橄欖鎮 | 果樹 | 蔬菜 | 花卉 | 農作物',
       // 'h1': '不知道農作物對應的季節嗎？煮飯時需要太多農作物很混亂嗎？試試看背包小助手吧',
       // 'h2': '背包小助手說明',
-      // 'p': '除了上方可以搜尋名稱功能外，下方也有種類和季節的標籤，可以只顯示出你想要的種類或季節，讓你快速找到它。點擊想要的農作物，小助手會記錄起來，接著可以點擊出現在右手邊的背包按鈕可以一覽你現在需要的農作物呦',
     },
     '/item':{
       // 'h1':  '物品太多不知道去哪收集嗎？這裡或許可以告訴你答案呦',
-      'h2': '',
+      // 'h2': '',
     },
     '/map': {
       'h1': '常常忘記到小鎮的時候要做哪些事嗎？快使用地圖小助手吧～',
-      'h2': '地圖小助手說明',
-      'p': '這裡沒搜尋功能，因為沒什麼好搜尋的ＸＤ，但可以點擊你想去的地方，甚至可以標記去那裡要幹嘛，讓小助手幫你記錄一下',
+      'h2': '點擊你想去的地方，甚至可以標記去那裡要幹嘛，讓小助手幫你記錄一下吧',
     },
     '/todo': {
       // 'h1': '溫馨提醒：今日事，今日畢',
       // 'h2': '代辦清單使用方式',
-      // 'p': '在其他頁面紀錄要做的事情，會全部集中顯示在這裡。也可以點擊右手邊的「+」按鈕新增自己的代辦事項呦。完成事項後，點擊該事項，就會通知對應的小助手把紀錄移除囉'
     },
   };
   if (descriptions[location.pathname]) {
@@ -44,13 +40,8 @@ function PageDescription() {
     }
     return (
     <>
-      <h1>{descriptions[location.pathname].h1}</h1>
-      { descriptions[location.pathname].h2 &&
-        <h2>
-          {descriptions[location.pathname].h2}
-          <span className='cursor-pointer' onClick={() => setShowDescription(!showDescription)}>{ showDescription ? ' ▲' : ' ▼'}</span>
-        </h2>}
-      { descriptions[location.pathname].p && showDescription && <p>{descriptions[location.pathname].p}</p>}
+      { descriptions[location.pathname].h1 && <h1>{descriptions[location.pathname].h1}</h1>}
+      { descriptions[location.pathname].h2 && <h2>{descriptions[location.pathname].h2}</h2>}
     </>);
   }
 
